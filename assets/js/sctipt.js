c477e1ce7598438afbbc5e31f5b457da8d0d636d
Yanakhmetov-Evgeniy <jenya2411199614@gmail.com>
2021-09-25 22:56:50 +0500

    footer-finish

--- assets/js/sctipt.js
+++ assets/js/sctipt.js
@@ -1,23 +1,20 @@
-function showMenu(nav, btn){
+function showMenu(nav, btn, hide){
    const navMenu = document.getElementById(nav);
    const navBtn = document.getElementById(btn);
 
    if(navMenu && navBtn){
        navBtn.addEventListener('click', function(){
-           navMenu.classList.toggle('active');
+           navMenu.classList.toggle(hide);
        })
    }   
 }
-showMenu('menu-nav', 'menu-btn');
+showMenu('menu-nav', 'menu-btn', 'active');
 
-function showMenu(discount, btn){
-   const navMenu = document.getElementById(discount);
-   const navBtn = document.getElementById(btn);
+showMenu('discount', 'discount-btn', 'discount_none');
+showMenu('header', 'discount-btn', 'header_padding_none');
+showMenu('menu-nav', 'discount-btn', 'menu_padding_none');
+showMenu('feautures', 'menu-btn', 'active' );
+showMenu('feautures', 'discount-btn', 'feautures_paddong_none');
+showMenu('btn-style', 'menu-btn', 'bx-x');
+showMenu('catalog', 'catalog__btn', 'catalog__max-width');
 
-   if(navMenu && navBtn){
-       navBtn.addEventListener('click', function(){
-           navMenu.classList.toggle('active');
-       })
-   }   
-}
-showMenu('discount', 'discount__btn_none');
